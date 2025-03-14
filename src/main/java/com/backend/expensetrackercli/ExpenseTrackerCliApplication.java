3b109b271a3f8788b97effaaffa4415b48c01c2c
Sukrut K Pasumarthi <kesarisukrut@gmail.com>
2025-03-14 13:59:42 -0400

    init

---
+++ src/main/java/com/backend/expensetrackercli/ExpenseTrackerCliApplication.java
@@ -0,0 +1,13 @@
+package com.backend.expensetrackercli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseTrackerCliApplication.class, args);
+    }
+
+}
